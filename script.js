1611d2e675489544862419239fd05a7f5664f399
Ankaleonova <ankaleonova@gmail.com>
2026-03-08 13:02:16 +0100

    correct and add 4 exerc.

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ teachers[4] = "Partick";
 console.log (fourthTeacher);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers.shift (6);
+const lastTeacher = teachers.pop (6);
 console.log (lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
