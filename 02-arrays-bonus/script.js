fee87c4356ffbba4c9a6d9f5e2949239e61ed985
Ankaleonova <ankaleonova@gmail.com>
2026-03-08 16:01:34 +0100

    2.exerc

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -21,8 +21,16 @@ console.log (reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for(let i - 0; i < teachers.length; i ++){
+  const thisTeacher = teachers[i]
+  console.log (thisTeacher.length)
+  if( thisTeacher.length >= 5){
+    longNames.push(thisTeacher)
+  }
 
+}
+console.log (longNames)
 // 3. Rimuovi 'Ed' dall'array teachers
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
