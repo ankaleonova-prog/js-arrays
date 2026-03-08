45b3f8fd98290286c0980df1b1efb1c6d3968c0b
Ankaleonova <ankaleonova@gmail.com>
2026-03-08 13:38:51 +0100

    1.exerc.

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -12,9 +12,12 @@ const teachers = [
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = [];
 for (let i = teachers.length - 1; i < 0 ; i++) {
-  console.log (reversedTeachers)
+  const thisteacher = teachers [i]
+  console.log (thiteacher)
+  reversedTeachers.push = thisteacher
 
 }
+console.log (reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
