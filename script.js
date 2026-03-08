f6fca9b564c1085a578ca97e0a000806b1b67234
Ankaleonova <ankaleonova@gmail.com>
2026-03-08 12:58:25 +0100

    3.exerc.

--- script.js
+++ script.js
@@ -14,9 +14,11 @@ const fourthTeacher = teachers[3];
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = "Partick";
+console.log (fourthTeacher);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = ;
+const lastTeacher = teachers.shift (6);
+console.log (lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
